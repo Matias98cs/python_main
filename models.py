04e0abe413b210a290d2de40698888f19d4869a0
matias_campos <matias98@gmail.com>
2023-08-02 21:12:15 -0300

    actualizacion con sqlalchemy

---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, TEXT, text, Date, case, func
+from sqlalchemy.orm import relationship, declarative_base
+from datetime import date
+
+
+Base = declarative_base()
+
+
+class Peticioneservidor(Base):
+    __tablename__ = 'peticioneservidor'
+    id = Column(Integer, primary_key=True)
+    instancia = Column(String(255))
+    estado = Column(Integer)
+    parametro1 = Column(String(255))
+    parametro2 = Column(TEXT)
+    fechainsercion = Column(Date)
+    fecha = Column(Date)
